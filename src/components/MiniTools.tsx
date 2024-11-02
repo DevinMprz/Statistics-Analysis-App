19f4897cc72398d699ab593c63badf8e83196603
Denys Tanashchuk <denistanasuk@gmail.com>
2024-11-02 17:04:13 +0100

    finished redesign + added responsiveness for phone screens

--- src/components/MiniTools.tsx
+++ src/components/MiniTools.tsx
@@ -5,28 +5,34 @@ const MiniTools = () => {
   return (
     <div className="mainMenuContainer">
       <div className="gridItem">
-        <h1>Minitool 1</h1>
-        <p>
-          Minitool 1 zobrazuje dve sady jednorozmerných (univariate) dát po max
-          10 až 12 bodov každá, ako „line graph“.
-        </p>
+        <div className="description">
+          <h1>Minitool 1</h1>
+          <p>
+            Minitool 1 zobrazuje dve sady jednorozmerných (univariate) dát po
+            max 10 až 12 bodov každá, ako „line graph“.
+          </p>
+        </div>
         <button>Minitool1</button>
       </div>
       <div className="gridItem">
-        <h1>Minitool 2</h1>
-        <p>
-          Minitool 2 zobrazuje dve sady jednorozmerných (univariate) dát po max
-          250 až 300 bodov každá, ako „dot plot“.
-        </p>
+        <div className="description">
+          <h1>Minitool 2</h1>
+          <p>
+            Minitool 2 zobrazuje dve sady jednorozmerných (univariate) dát po
+            max 250 až 300 bodov každá, ako „dot plot“.
+          </p>
+        </div>
         <button>Minitool2</button>
       </div>
       <div className="gridItem">
-        <h1>Minitool 3</h1>
-        <p>
-          Minitool 3 zobrazuje sady dvojrozmerných (bivariate) dát,
-          preklikávateľné cez pull-down menu, pričom ostanú zapnuté rovnaké
-          nástroje.
-        </p>
+        <div className="description">
+          <h1>Minitool 3</h1>
+          <p>
+            Minitool 3 zobrazuje sady dvojrozmerných (bivariate) dát,
+            preklikávateľné cez pull-down menu, pričom ostanú zapnuté rovnaké
+            nástroje.
+          </p>
+        </div>
         <button>Minitool3</button>
       </div>
     </div>

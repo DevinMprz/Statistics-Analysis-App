07c60f332d89c34c7217b66feca05c1cd5043869
Denys Tanashchuk <denistanasuk@gmail.com>
2024-11-02 16:32:37 +0100

    redesign main menu

--- src/components/MiniTools.tsx
+++ src/components/MiniTools.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import './minitools.css';
+import React from "react";
+import "./minitools.css";
 
 const MiniTools = () => {
   return (
-    <div className="GridContainer">
-      <div className="GridItem">
+    <div className="mainMenuContainer">
+      <div className="gridItem">
+        <h1>Minitool 1</h1>
         <p>
           Minitool 1 zobrazuje dve sady jednorozmerných (univariate) dát po max
           10 až 12 bodov každá, ako „line graph“.
         </p>
         <button>Minitool1</button>
       </div>
-      <div className="GridItem">
+      <div className="gridItem">
+        <h1>Minitool 2</h1>
         <p>
           Minitool 2 zobrazuje dve sady jednorozmerných (univariate) dát po max
           250 až 300 bodov každá, ako „dot plot“.
         </p>
         <button>Minitool2</button>
       </div>
-      <div className="GridItem">
+      <div className="gridItem">
+        <h1>Minitool 3</h1>
         <p>
           Minitool 3 zobrazuje sady dvojrozmerných (bivariate) dát,
           preklikávateľné cez pull-down menu, pričom ostanú zapnuté rovnaké

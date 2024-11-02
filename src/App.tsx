61970d70aca669a42a0c5fd21e5f2f6ecd317f69
EndiUN <endi.ungamer@gmail.com>
2024-11-02 14:54:30 +0100

    add React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import MiniTools from './components/MiniTools';
+
+let a: String | number = 5;
+
+type Person = {
+  age:number;
+  name:string;
+};
+
+
+const App:React.FC = () => {
+  return (
+    <div className="App">
+      <span className='heading'>Data Analysis</span>
+      <MiniTools />
+    </div>
+  );
+}
+
+export default App;
